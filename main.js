e78d98920b246a0bf346b3f4bcac0aea1a51f4d2
JeanaDeLaire <jeanadelaire@gmail.com>
2018-10-15 13:46:51 -0400

    First Commit
    Contains basic design and javascript arrays.

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Simon Says, look I'm working!")
+
+// Step 1: Random sequence should be setup and work when start button is clicked
+
+var game = [
+{
+colors: ['#red', '#blue', '#green', '#yellow'],
+gameCurrent: []
+sound: ['simonsound/red.mpe3', 'simonsound/blue.mp3', 'simonsound/green.mp3', 'simonsound.yellow.mp3']
+];
+
+
+
+document.getElementById("myDiv").style.backgroundColor = "lightblue";
+
+
+// Step 2: User replicates sequence 
+
+// Scorekeeper tracks score
+
+// 
+
+//
+//
+//
